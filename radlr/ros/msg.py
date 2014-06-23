bb58eddbdf7b8e310d655b301e4607fc496270f1
Léonard Gérard <leonard.gerard@sri.com>
2014-06-23 11:39:22 -0700

    struct generation

--- radlr/ros/msg.py
+++ radlr/ros/msg.py
@@ -4,14 +4,15 @@ Created on May, 2014
 @author: Léonard Gérard leonard.gerard@sri.com
 
 
-Generate one ROS .msg file per topic declaration.
+Generate one ROS .msg file per topic and struct declaration.
 
 '''
+
 from radlr.rast import AstVisitor
 from astutils.tools import write_file
 
 def _topic(visitor, node, acc):
-    """ Topics are not recursive """
+    """ Topics needs a radl__flags field. """
     filenames, msg_directory = acc
     filename = node._name + '.msg'
     filenames.append(filename)
@@ -20,10 +21,21 @@ def _topic(visitor, node, acc):
     for n in node['FIELDS']:
         filecontent += '{} {}\n'.format(n._kind, n._name)
     write_file(filepath, filecontent)
-    return (), (filenames, msg_directory)
+    return visitor.node_mapacc(node, acc) #recursive call
+
+def _struct(visitor, node, acc):
+    filenames, msg_directory = acc
+    filename = node._name + '.msg'
+    filenames.append(filename)
+    filepath = msg_directory / filename
+    filecontent = ''
+    for n in node['S']:
+        filecontent += '{} {}\n'.format(n._kind, n._name)
+    write_file(filepath, filecontent)
+    return visitor.node_mapacc(node, acc) #recursive call
 
 
-_visitor = AstVisitor({'topic' : _topic})
+_visitor = AstVisitor({'topic' : _topic, 'struct' : _struct})
 
 def gen(msg_directory, ast):
     _, (msg_file_list, _) = _visitor.visit(ast, ([], msg_directory))
