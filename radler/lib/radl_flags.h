a6e002c4386bf9114ac8bc99724527827827b3f0
Léonard Gérard <leonard.gerard@sri.com>
2014-07-03 18:50:32 -0700

    Better documentation of flag handling.

--- radler/lib/radl_flags.h
+++ radler/lib/radl_flags.h
@@ -24,11 +24,17 @@ const flags_t STALE = 1;
 const flags_t FAILURE_FLAGS = ~FUNCTIONAL_FLAG;
 const flags_t TIMEOUT = 16;
 
-// Handy user shortcuts
+// Handy user functions
+
+// return if the stale flag of f is on
 inline bool is_stale(flags_t f) { return f & STALE; };
+// return if the timeout flag of f is on
 inline bool is_timeout(flags_t f) { return f & TIMEOUT; };
+
+// return true if any failure flag of f is on
 inline bool is_failing(flags_t f) { return f & FAILURE_FLAGS; };
 
+// Flags manipulation, ex: turn_on(STALE, x) turn on the stale flag of x
 inline void turn_on(flags_t f, flags_t & x) { x |= f; };
 inline void turn_off(flags_t f, flags_t & x) { x &= ~f; };
 
