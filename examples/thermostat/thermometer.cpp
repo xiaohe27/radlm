40261d80985846ecd29eea42a8971688c07edce9
Leonard Gerard <leonard.gerard@sri.com>
2014-07-09 08:00:12 -0700

    stubing the thermostat to compile

--- examples/thermostat/thermometer.cpp
+++ examples/thermostat/thermometer.cpp
@@ -4,8 +4,6 @@
 
 void Thermometer::step(const _in_t * in, const _in_flags_t* inflags,
                        _out_t * out, _out_flags_t* outflags){
-  float noise = (float)(rand()%20000 - 10000)/10000;
-  out->thermometer_temp->temp = in->house_temp->temp + noise;
 }
 
 
--- examples/thermostat/thermostat.h
+++ examples/thermostat/thermostat.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "thermostat_node.h"
 
 class Thermostat {
