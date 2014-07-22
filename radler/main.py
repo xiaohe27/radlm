7c58f27aa5bd6d98c8a074499e3d173a6190f2fa
Léonard Gérard <leonard.gerard@sri.com>
2014-07-22 14:23:01 -0400

    wip

--- radler/main.py
+++ radler/main.py
@@ -9,63 +9,60 @@ from pathlib import Path
 
 from radler.radlr import crossrefs, pwds, errors, arrays, infos, alias,\
     language
-from radler.radlr.errors import log_err
+from radler.radlr.errors import log_err, log3
 from radler.radlr.parser import Semantics
 from radler.radlr.ros import *
+import pickle
+import marshal
 
 ########
 # Parse arguments
 ########
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', help='the RADL description file')
-parser.add_argument('--dest', default='src',
-    help='the destination directory for generated files')
-verbgroup = parser.add_mutually_exclusive_group()
-verbgroup.add_argument('--silent', dest='verb', action='store_const', const=-1,
-    default=0, help='set verbosity to -1')
-verbgroup.add_argument('--verbosity', type=int, dest='verb', default=0,
-    help='verbosity level, negative removes warnings, '
-         'positive gives additional informations, '
-         'level 2 and higher are mostly for debugging purposes.')
-verbgroup.add_argument('--verbose', '-v', dest='verb', action='count',
-    help='increase verbosity by 1')
-parser.add_argument('--warning_as_errors', action='store_true')
-parser.add_argument('--continue_when_errors', action='store_true')
-#TODO: 8 flags to decide the generation
-#parser.add_argument('--gen', action='accumulate', default='ROS')
+p = argparse.ArgumentParser()
 
-args = parser.parse_args()
+p.add_argument('radl_file', help='the RADL source file')
+p.add_argument('object_files', nargs='*', help='compiled object files')
+vgrp = p.add_mutually_exclusive_group()
+vgrp.add_argument('--silent', dest='verb', action='store_const', const=-1, default=0, help='set verbosity to -1')
+vgrp.add_argument('--verbosity', type=int, dest='verb', default=0, help='verbosity level, negative removes warnings, positive gives additional informations, level 2 and higher are mostly for debugging purposes.')
+vgrp.add_argument('--verbose', '-v', dest='verb', action='count', help='increase verbosity by 1')
+p.add_argument('--warning_as_errors', action='store_true', help='warnings are treated as errors')
+p.add_argument('--continue_when_errors', action='store_true', help='tries to recover from errors')
+p.add_argument('-c', '--compile', help='generate an object file for each source file')
+p.add_argument('-o', '--output', default='.', help='generated object file path')
+p.add_argument('--roscpp_dest', help='generate roscpp files in the dest')
+p.add_argument('--force_bootstrap', action='store_true', help='force regeneration of the p')
+
+args = p.parse_args()
 
 errors.continue_when_errors = args.continue_when_errors
 errors.warning_as_errors = args.warning_as_errors
 errors.verbosity_level = args.verb
 
-if not args.file:
-    import tkinter.filedialog
-    tkinter.Tk().withdraw() # Close the root window
-    args.file = tkinter.filedialog.askopenfilename()
 
-source = Path(args.file).resolve()
+source = Path(args.radl_file).resolve()
 if not source.is_file():
     log_err("The source file {} doesn't exists.".format(source))
     exit(-1)
 infos.source_file = source
 
-dest_dir = Path(args.dest)
+
+dest_dir = Path(args.roscpp_dest)
 if not dest_dir.is_dir():
     log_err("The destination directory {} doesn't exists.".format(dest_dir))
     exit(-2)
 infos.dest_dir = dest_dir
 
 ########
-# Bootstrap the semantics from the language definition
+# Bootstrap the semantics from the language definition if needed.
 ########
 
 infos.semantics = Semantics(language)
 
 # #saving the bootsrap is not possible since pickle doesn't pickle functions.
-# #maybe by mixing pickling and marshaling of __code__ of the functions
+# #so... tree_to_ast and ast_checker are not pickable.
+# #Maybe by mixing pickling and marshaling of __code__ of the functions
 # #is possible, but for now....
 # sem_file = infos.script_dir / 'semantics.pickle'
 # try:
@@ -81,12 +78,15 @@ infos.semantics = Semantics(language)
 #         pickle.dump(infos.semantics, f)
 
 ########
+# Load object files
+########
+
+########
 # Parse
 ########
 qname = infos.root_namespace.qualify(source.stem)  # @UndefinedVariable
 with source.open() as f:
     infos.ast = infos.semantics(f.read(), qname, infos.root_namespace)
-
 
 
 ######## Freeze #######
@@ -105,9 +105,18 @@ alias.make_transparent(infos.ast, infos.root_namespace)
 crossrefs.add(infos.ast)
 pwds.add(infos.ast)
 
+
+
+# with open('/tmp/ast.pickle', 'wb') as f:
+#     pickle.dump(infos.ast, f)
+# with open('/tmp/ast.pickle', 'rb') as f:
+#     ast = pickle.load(f)
+
+ast = infos.ast
+
 # ROS files generation
-rosutils.gen_dirs(infos.ast)
-msg_list = rosmsg.gen(infos.ast)
-gened_cpp_files = rosnode.gen(infos.ast)
-rospackagexml.gen(infos.ast)
-roscmake.gen(msg_list, gened_cpp_files, infos.ast)
+rosutils.gen_dirs(ast)
+msg_list = rosmsg.gen(ast)
+gened_cpp_files = rosnode.gen(ast)
+rospackagexml.gen(ast)
+roscmake.gen(msg_list, gened_cpp_files, ast)
