b42ab593ff37d3699093b6d0c7273a79a8d8d89e
Léonard Gérard <leonard.gerard@sri.com>
2014-07-25 15:44:01 -0400

    basic test with output to display use of flags

--- examples/test2/f2.h
+++ examples/test2/f2.h
@@ -2,11 +2,13 @@
 #include "n2_node.h"
 using namespace std;
 using namespace radl;
+
 class N2 {
 public:
+
   void step(const _in_t* i, const _in_flags_t* f,
         _out_t* o, _out_flags_t* of) {
-//    std::cout << i->i2->x1 << " " << is_stale(f->i2) <<" "<< is_timeout(f->i2) << endl;
+    std::cout << i->i2->x1 << " " << is_stale(f->i2) <<" "<< is_timeout(f->i2) << endl;
   }
 };
 
